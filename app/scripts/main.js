b155f9d9c4c58892f99cac100040037e8bd3dd6d
Eduardo Jimenez <worldjiz@gmail.com>
2016-05-20 17:18:15 -0500

    Agreagado de modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
 	$('.parallax').parallax();
 	$('.slider').slider({full_width: true});
 	$('.carousel').carousel();
+	$('.modal-trigger').leanModal();
 
 
 	particlesJS('particles-js',
